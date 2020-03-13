9e4ebe3ef56e33e8916f5e288dcd3956f53bc4eb
Justin Dwyer <justindwyer6@gmail.com>
2020-03-12 21:29:34 -0700

    removing random internet cors tip

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const express = require("express")
 const app = express()
 
 app.use(express.static(__dirname))
-// app.set('views', __dirname)
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html')
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/day-0-index/index.html")
