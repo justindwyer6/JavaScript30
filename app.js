3fe458fbbf53c32565bc5b41454df3522d3c1c9a
Justin Dwyer <justindwyer6@gmail.com>
2020-03-29 14:49:32 -0700

    starting video player

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 // import express from 'express'
 const express = require("express"),
-      app = express(),
-      fs = require("fs")
+  app = express(),
+  fs = require("fs");
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
-const allDirectories = fs.readdirSync(__dirname)
+const allDirectories = fs.readdirSync(__dirname);
 
-dayRegEx = new RegExp(/day-\d+-/)
-const dayDirectories = allDirectories.filter((directory) => directory.match(dayRegEx))
-const projectNames = dayDirectories.map((directory, i) => directory.replace(dayRegEx, ""))
+dayRegEx = new RegExp(/day-\d+-/);
+const dayDirectories = allDirectories.filter(directory =>
+  directory.match(dayRegEx)
+);
+const projectNames = dayDirectories.map((directory, i) =>
+  directory.replace(dayRegEx, "")
+);
 
 dayDirectories.forEach((project, day) => {
-    app.get(`/${projectNames[day] === "index" ? "" : projectNames[day]}`, (req, res) => {
-        res.sendFile(`${__dirname}/${project}/${projectNames[day]}.html`)
-    })
-})
+  app.get(
+    `/${projectNames[day] === "index" ? "" : projectNames[day]}`,
+    (req, res) => {
+      res.sendFile(`${__dirname}/${project}/${projectNames[day]}.html`);
+    }
+  );
+});
 
-let port = process.env.PORT
-port = (port == null || port == "") ? 3000 : port
-app.listen(port, () => {console.log(`Ya, I hear ya.`)})
+let port = process.env.PORT;
+port = port == null || port == "" ? 3000 : port;
+app.listen(port, () => {
+  console.log(`Ya, I hear ya.`);
+});
