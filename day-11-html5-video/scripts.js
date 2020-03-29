8dbac4229102ed40fd85896020f8fcd8c9bc5502
Justin Dwyer <justindwyer6@gmail.com>
2020-03-29 15:52:46 -0700

    video's done. Adding fullscreen, mute, speed buttons

--- day-11-html5-video/scripts.js
+++ day-11-html5-video/scripts.js
@@ -1,4 +1,4 @@
-// Get our elements
+/* Get our elements */
 const player = document.querySelector('.player')
 const video = player.querySelector('.viewer')
 const progress = player.querySelector('.progress')
@@ -7,7 +7,7 @@ const toggle = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip]')
 const ranges = player.querySelectorAll('.player__slider')
 
-// Build out functions
+/* Build out functions */
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause'
   video[method]()
@@ -27,11 +27,17 @@ function handleRangeUpdate() {
 }
 
 function handleProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = `${percent}%`;
+  const percent = (video.currentTime / video.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
 }
 
 /* Add event listeners */
+
 // Play on clicks
 video.addEventListener('click', togglePlay)
 toggle.addEventListener('click', togglePlay)
@@ -40,5 +46,14 @@ video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 // Skip logic
 skipButtons.forEach(button => button.addEventListener('click', skip))
+// Speed and Volume Sliders
 ranges.forEach(button => button.addEventListener('change', handleRangeUpdate))
 ranges.forEach(button => button.addEventListener('mousemove', handleRangeUpdate))
+// Video Progress Indicator/Scubber
+video.addEventListener('timeupdate', handleProgress)
+
+let mousedown = false
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener('mousedown', () => mousedown = true)
+progress.addEventListener('mouseup', () => mousedown = false)
